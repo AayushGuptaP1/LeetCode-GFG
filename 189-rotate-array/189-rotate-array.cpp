6cad057baa00c3d53640cf0bdb43b6ce58e656a1
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2022-09-22 13:00:57 +0530

    Time: 83 ms (7.96%), Space: 25.1 MB (58.92%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -6,7 +6,8 @@ public:
         
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k, nums.end());       
+        reverse(nums.begin()+k, nums.end()); 
+         
     }
    
 };
