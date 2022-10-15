2d29ac48638bf5354e5e52d3b59abbdaa9bc0b32
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2022-10-15 19:27:43 +0530

    Time: 3 ms (92.17%), Space: 12.1 MB (36.64%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int i = n-2;
+        int j = n-1;
+        
+        while(i>=0 && nums[i]>=nums[i+1])
+        {
+            i--;
+        }
+        if(i<0)
+        {
+            reverse(nums.begin(),nums.end());
+        }
+        else
+        {
+            while(j>i && nums[j]<=nums[i])
+            {
+                j--;
+            }
+            swap(nums[i],nums[j]);
+            reverse(nums.begin()+i+1,nums.end());
+        }
+       
+        
+    }
+};
