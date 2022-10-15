6790dfdb86ca0aa2de4404594d31120db60844a3
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2022-10-15 23:44:52 +0530

    Time: 843 ms (15.38%), Space: 140.1 MB (100.00%) - LeetHub

---
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> productQueries(int n, vector<vector<int>>& queries) 
+    {
+        const unsigned int M = 1000000007;
+        
+
+        int count = 1;
+        vector<int> v;
+        vector<int> power(queries.size(),1);
+        
+        while(n)
+        {
+            if((n & 1) == 1)
+                v.push_back(count);
+            count *= 2;
+            n = n>>1;
+        }
+        
+        
+        for(int i = 0; i<queries.size();i++)
+        {
+            long long temp = 1;
+            for(int j = queries[i][0]; j<= queries[i][1];j++)
+            {
+               temp = ((temp%M)*(v[j]%M))%M;                 
+            }
+            power[i] = temp;
+              
+        }
+        return power;
+    }
+};
