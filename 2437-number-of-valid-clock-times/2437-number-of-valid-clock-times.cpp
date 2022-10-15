5e14d182343417b871bb3ec850b548ea3bcec9e8
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2022-10-15 23:31:36 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (54.55%) - LeetHub

---
+++ 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int countTime(string time) 
+    {
+       int res = 1;
+        
+        if(time[4] == '?')
+            res *= 10;
+        if(time[3] == '?')
+            res *= 6;
+        
+        if(time[0] == '?' && time[1] == '?')
+            res *= 24;
+        else if(time[0] == '?' && time[1] != '?')
+        {
+            if((time[1] == '0') ||(time[1] =='1') || (time[1] == '2') || (time[1] == '3'))
+                res *= 3;
+            else
+                res *= 2;                    
+        }
+            
+        else if(time[0] != '?' && time[1] == '?')
+        {
+            if((time[0] == '0') || (time[0] == '1'))
+               res *= 10;
+            else
+               res *= 4;
+        }
+        
+            
+       
+        return res;
+    }
+};
