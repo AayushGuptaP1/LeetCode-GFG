3c7064e2166e94961ea42c73593365038dc51b34
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2022-10-16 22:31:11 +0530

    Time: 6 ms (72.22%), Space: 11.3 MB (31.98%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -9,10 +9,14 @@ public:
 	{
 		for(int j=0;j<n;j++)
 		{
-			if(mat[i][j]!=target[i][j]) c[0]=false;
-			if(mat[i][j]!=target[n-j-1][i]) c[1]=false;
-			if(mat[i][j]!=target[n-i-1][n-j-1]) c[2]=false;
-			if(mat[i][j]!=target[j][n-i-1]) c[3]=false;
+            if(mat[i][j]!=target[i][j])
+		            c[0] = false;
+            if(mat[i][j]!=target[j][n-i-1])
+                 c[1] = false;
+			if(mat[i][j]!=target[n-i-1][n-j-1])
+                c[2] = false;
+			if(mat[i][j]!=target[n-j-1][i])
+                c[3] = false;
 		}
 	}
 	return c[0]||c[1]||c[2]||c[3];
