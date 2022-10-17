5a0006d958dad7f644af6c34817a1c98df421540
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2022-10-17 07:55:39 +0530

    Time: 4 ms (35.13%), Space: 5.9 MB (47.05%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double myPow(double x, int n) 
+    {
+        double ans = 1.0;
+        long long nn = n;
+        if(nn<0)
+            nn = -1*nn;
+        
+        while(nn>0)
+        {
+            if(nn%2==1)
+            {
+                ans *= x;
+                nn--;
+            }
+            else
+            {
+                x *= x;
+                nn /= 2;
+            }                
+        }
+        if(n<0)
+            ans = (double)1.0/(double)ans;
+        return ans;
+    }
+};
