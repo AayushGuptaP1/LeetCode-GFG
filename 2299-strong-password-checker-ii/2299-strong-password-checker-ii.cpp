70ba0b49e59df367dde2371dcfc6022edf827e84
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2022-10-26 11:16:40 -0700

    Time: 0 ms (100.00%), Space: 6.1 MB (52.56%) - LeetHub

--- 2299-strong-password-checker-ii/2299-strong-password-checker-ii.cpp
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.cpp
@@ -4,23 +4,24 @@ public:
     {
         int n = password.length();
         if(n<8)return false;
+        
         bool lower = false, upper = false, digit = false, special = false;
-   int count = 0;
+        int count = 0;
    
-   for(int i = 0;i<n; ++i)
-   {
-       if(i<n-1 && password[i] == password[i+1])
-           return false;
-       if(password[i]>='A' && password[i]<='Z')
-       upper = true;
-       else if (password[i]>='a' && password[i]<='z')
-       lower = true;
-       else if(password[i]>='0' && password[i]<= '9')
-       digit = true;
-       else
-       special = true;
-   }
-   return(lower&&upper&&digit&&special);
+        for(int i = 0;i<n; ++i)
+        {
+           if(i<n-1 && password[i] == password[i+1])
+               return false;
+           if(password[i]>='A' && password[i]<='Z')
+           upper = true;
+           else if (password[i]>='a' && password[i]<='z')
+           lower = true;
+           else if(password[i]>='0' && password[i]<= '9')
+           digit = true;
+           else
+           special = true;
+        }
+        return(lower&&upper&&digit&&special);
    
 
    
