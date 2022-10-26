5c1fba7644a20cf8c7e53d2848630e18b69ee754
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2022-10-26 11:08:08 -0700

    Time: 3 ms (67.32%), Space: 6.5 MB (6.54%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int numJewelsInStones(string jewels, string stones) 
+    int numJewelsInStones(string &jewels, string &stones) 
     {
         unordered_set<char>st(jewels.begin(),jewels.end());
         int count = 0;
