f842a54fad31d929024ea513b18f1afc9a2dbbf0
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2022-11-09 10:15:02 -0800

    Time: 52 ms (57.00%), Space: 23.9 MB (87.52%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -7,17 +7,14 @@ public:
         int preProd = 1;
         int postProd = 1;
         
-        for(int i=1;i<n;++i)
+        for(int i=1; i<n; ++i)
         {
-            res[i] = res[i-1]* nums[i-1];
-            preProd *= nums[i];
+            res[i] = nums[i-1]*res[i-1];
         }
-        
-        for(int i=n-1;i>=0;--i)
+        for(int i = n-1;i>=0;--i)
         {
             res[i] *= postProd;
             postProd *= nums[i];
-            
         }
         return res;
     }
