3475efa1fa018fb5269f3bbd563ab34d96fa6263
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2022-11-10 00:54:03 -0800

    Time: 168 ms (70.52%), Space: 41.5 MB (48.42%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -5,19 +5,17 @@ public:
         int n = nums.size();
         unordered_map<int,int> mp;
         int ans = 0;
-        int sum =  0;
+        int sum = 0;
         mp[sum]++;
         
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;++i)
         {
             sum += nums[i];
             
-            if(mp.find(sum-k)!= mp.end())
+            if(mp.find(sum-k) != mp.end())
                 ans += mp[sum-k];
             mp[sum]++;
         }
         return ans;
-        
-        
     }
 };
