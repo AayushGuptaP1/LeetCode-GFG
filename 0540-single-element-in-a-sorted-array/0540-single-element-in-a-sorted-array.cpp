ab03d4f9ae676bfec290945a129bf1a234866a4e
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2022-11-09 21:33:26 -0800

    Time: 88 ms (12.17%), Space: 22.2 MB (99.77%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -7,23 +7,12 @@ public:
         while(low<high)
         {
             int mid = (low+high)>>1;
-            if(nums[mid]!=nums[mid-1] && nums[mid] != nums[mid+1])
-                return nums[mid];
             
-            else if(mid%2==0)
-            {
-                if(nums[mid]==nums[mid-1])
-                    high = mid-2;
-                else
-                    low = mid+2;
-            }
+            if(nums[mid]==nums[mid^1])
+                low = mid+1;
             else
-            {
-                if(nums[mid]==nums[mid-1])
-                    low = mid+1;
-                else
-                    high = mid-1;
-            }
+                high = mid;
+                
         }
         return nums[low];
     }
