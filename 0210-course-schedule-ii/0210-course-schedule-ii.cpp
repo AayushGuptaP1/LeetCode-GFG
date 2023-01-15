e75906d4422438fb43800d1e70c770eb3fb10e23
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-01-15 09:56:16 -0800

    Time: 22 ms (85.92%), Space: 13.5 MB (81.80%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+        vector<int> topSort;
+        vector<vector<int>>graph(numCourses);
+        vector<int>dep(numCourses);
+        queue<int> q;
+        int count = 0;
+        
+        for(auto &u : prerequisites)
+        {
+            graph[u[1]].push_back(u[0]);
+            dep[u[0]]++;            
+        }
+        
+        for(int i=0;i<numCourses;++i)
+        {
+            if(dep[i]==0)
+            {
+                q.push(i);
+                ++count;
+            }
+                
+        }
+        
+        while(q.empty()==false)
+        {
+            int temp = q.front();
+            q.pop();
+            topSort.push_back(temp);
+            
+            for(int i=0;i<graph[temp].size();++i)
+            {
+                --dep[graph[temp][i]];
+                if(dep[graph[temp][i]]==0)
+                {
+                    q.push(graph[temp][i]);
+                    ++count;
+                }
+                    
+            }
+        }
+        if(topSort.size()==numCourses)
+            return topSort; 
+        else
+        return {};
+        
+            
+            
+        
+        
+        
+            
+    }
+};
