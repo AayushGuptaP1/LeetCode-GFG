cb7fa8765104cf0a21f6382596f0950218465113
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-01-15 09:02:06 -0800

    Time: 26 ms (71.88%), Space: 13.3 MB (81.94%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,44 @@
+class Solution {
+
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+        vector<vector<int>>graph(numCourses);
+        vector<int> inDegree(numCourses,0);
+        queue<int>q;
+        
+        for(auto &e : prerequisites)
+        {
+            graph[e[1]].push_back(e[0]);
+            inDegree[e[0]]++;
+        }
+        
+        int count = 0;
+        
+        for(int i=0;i<numCourses;++i)
+        {
+            if(inDegree[i]==0)
+            {
+                q.push(i);
+                ++count;                
+            }                               
+        }
+        while(q.empty()==false)
+        {
+            int temp = q.front();
+            q.pop();
+            
+            for(int i=0;i<graph[temp].size();++i)
+            {
+                inDegree[graph[temp][i]]--;
+                if(inDegree[graph[temp][i]]==0)
+                {
+                    q.push(graph[temp][i]);
+                    ++count;
+                }                     
+            }
+        }
+          return count==numCourses;     
+        
+    }
+};
