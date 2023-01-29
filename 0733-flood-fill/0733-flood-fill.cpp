095278e8ef52baf3e6ce24511a23a857c1d2b545
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-01-29 07:32:44 -0800

    Time: 13 ms (49.02%), Space: 14.3 MB (27.23%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,47 +1,41 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color)
     {
         int m = image.size();
         int n = image[0].size();
-        
+        int source = image[sr][sc];
+        image[sr][sc] = color;
         queue<pair<int,int>>q;
-        vector<vector<bool>>visited(m,vector<bool>(n,false));
         q.push({sr,sc});
-        visited[sr][sc] = true;
         
-        while(q.empty() == false)
+        while(q.empty()==false)
         {
             int row = q.front().first;
             int col = q.front().second;
             q.pop();
             
-            if(row-1>=0 && row-1<m && image[row-1][col]==image[sr][sc] && visited[row-1][col] == false)
+            if(row-1>=0 && row-1<m && image[row-1][col] == source && image[row-1][col] != color)
             {
                 image[row-1][col] = color;
-                visited[row-1][col] = true;
                 q.push({row-1,col});
             }
-            if(row+1>=0 && row+1<m && image[row+1][col]==image[sr][sc] && visited[row+1][col] == false)
+            if(row+1>=0 && row+1<m && image[row+1][col] == source && image[row+1][col] != color)
             {
                 image[row+1][col] = color;
-                visited[row+1][col] = true;
                 q.push({row+1,col});
             }
-            if(col-1>=0 && col-1<n && image[row][col-1]==image[sr][sc] && visited[row][col-1] == false)
+            if(col-1>=0 && col-1<n && image[row][col-1] == source && image[row][col-1] != color)
             {
                 image[row][col-1] = color;
-                visited[row][col-1] = true;
                 q.push({row,col-1});
             }
-            if(col+1>=0 && col+1<n && image[row][col+1]==image[sr][sc] && visited[row][col+1] == false)
+            if(col+1>=0 && col+1<n && image[row][col+1] == source && image[row][col+1] != color)
             {
                 image[row][col+1] = color;
-                visited[row][col+1] = true;
                 q.push({row,col+1});
-            }
+            }  
         }
-        image[sr][sc] = color;
         return image;
     }
 };
