2297877cebf0c5cebcadd281ed52539773c293b3
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-02-03 07:34:19 -0800

    Time: 87 ms (76.48%), Space: 32.2 MB (50.16%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,7 +4,7 @@ public:
     {
         int m = mat.size();
         int n = mat[0].size();
-        queue<pair<pair<int,int>,int>>q;
+        queue<pair<int,int>>q;
         vector<vector<bool>>visited(m,vector<bool>(n));
         
         for(int i=0;i<m;++i)
@@ -13,7 +13,7 @@ public:
             {
                 if(mat[i][j]==0)
                 {
-                    q.push({{i,j},0});
+                    q.push({i,j});
                     visited[i][j] = true;                    
                 }                 
             }
@@ -21,37 +21,37 @@ public:
         
         while(q.empty()==false)
         {
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int time = q.front().second;
+            int row = q.front().first;
+            int col = q.front().second;
+            
             q.pop();
             
             if(row-1>=0 && row-1<m && visited[row-1][col]==false)
             {
-                mat[row-1][col] = time+1;
+                mat[row-1][col] = mat[row][col]+1;
                 visited[row-1][col] = true;
-                q.push({{row-1,col},time+1});
+                q.push({row-1,col});
                 
             }
             if(row+1>=0 && row+1<m && visited[row+1][col]==false)
             {
-                mat[row+1][col] = time+1;
+                mat[row+1][col] = mat[row][col]+1;
                 visited[row+1][col] = true;
-                q.push({{row+1,col},time+1});
+                q.push({row+1,col});
                 
             }
             if(col-1>=0 && col-1<n && visited[row][col-1]==false)
             {
-                mat[row][col-1] = time+1;
+                mat[row][col-1] = mat[row][col]+1;
                 visited[row][col-1] = true;
-                q.push({{row,col-1},time+1});
+                q.push({row,col-1});
                 
             }
             if(col+1>=0 && col+1<n && visited[row][col+1]==false)
             {
-                mat[row][col+1] = time+1;
+                mat[row][col+1] = mat[row][col]+1;
                 visited[row][col+1] = true;
-                q.push({{row,col+1},time+1});
+                q.push({row,col+1});
                 
             }
         }
