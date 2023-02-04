d9243c40fb64f52dc20321818fcc17c3feedb2a7
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-02-04 06:48:22 -0800

    Time: 15 ms (65.94%), Space: 9.7 MB (99.60%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -11,31 +11,7 @@ public:
     
     void solve(vector<vector<char>>& board) {
       
-      //We will use boundary DFS to solve this problem
-        
-      // Let's analyze when an 'O' cannot be flipped,
-      // if it has atleast one 'O' in it's adjacent, AND ultimately this chain of adjacent 'O's is connected to some 'O' which lies on boundary of board
-        
-      //consider these two cases for clarity :
-      /*
-        O's won't be flipped          O's will be flipped
-        [X O X X X]                   [X X X X X]     
-        [X O O O X]                   [X O O O X]
-        [X O X X X]                   [X O X X X] 
-        [X X X X X]                   [X X X X X]
       
-      So we can conclude if a chain of adjacent O's is connected some O on boundary then they cannot be flipped
-      
-      */
-        
-      //Steps to Solve :
-      //1. Move over the boundary of board, and find O's 
-      //2. Every time we find an O, perform DFS from it's position
-      //3. In DFS convert all 'O' to '#'      (why?? so that we can differentiate which 'O' can be flipped and which cannot be)   
-      //4. After all DFSs have been performed, board contains three elements,#,O and X
-      //5. 'O' are left over elements which are not connected to any boundary O, so flip them to 'X'
-      //6. '#' are elements which cannot be flipped to 'X', so flip them back to 'O'
-      //7. finally, Upvote the solution\U0001f60a   
         
       
      int m = board.size();
@@ -44,7 +20,7 @@ public:
         
      int n = board[0].size();
      
-     //Moving over firts and last column   
+     
      for(int i=0; i<m; i++) {
          if(board[i][0] == 'O')
              DFS(board, i, 0, m, n);
@@ -53,7 +29,7 @@ public:
      }
         
         
-     //Moving over first and last row   
+       
      for(int j=0; j<n; j++) {
          if(board[0][j] == 'O')
              DFS(board, 0, j, m, n);
