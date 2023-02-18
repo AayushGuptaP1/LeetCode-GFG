ca5b4c7b9298dea34876a585fe9e000a4de0919b
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-02-18 11:04:22 -0800

    Time: 165 ms (74.22%), Space: 46.8 MB (95.59%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,9 +1,9 @@
 class Solution {
 private:
-bool dfs(int u,vector<vector<int>>& graph,vector<bool>&visited,vector<int> &recSt)
+bool dfs(int u,vector<vector<int>>& graph,vector<bool>&visited,vector<bool> &recSt)
 {
     visited[u] = true;
-    recSt[u] = 1;
+    recSt[u] = true;
     
     for(auto &x : graph[u])
     {
@@ -15,16 +15,10 @@ bool dfs(int u,vector<vector<int>>& graph,vector<bool>&visited,vector<int> &recS
         else if(recSt[x]==1)
             return false;
     }
-    recSt[u] = 0;
+    recSt[u] = false;
     return true;
 }
-    
-        
-    
-    
-    
-            
-             
+      
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph)
     {
@@ -32,7 +26,7 @@ public:
         vector<int> res;
         
         vector<bool> visited(m,false);
-        vector<int> recSt(m,0);
+        vector<bool> recSt(m);
         
         for(int i=0;i<m;++i)
         {
@@ -40,7 +34,7 @@ public:
                 dfs(i,graph,visited,recSt);
         }
         for(int i=0;i<m;++i)
-            if(recSt[i]==0)
+            if(recSt[i]==false)
                 res.push_back(i);
         return res;
     }
