7458e458b80d074dde9752a22b46b1f4932794ae
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-03-05 09:23:18 -0800

    Added solution - LeetHub

---
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        vector<int> distance(V,INT_MAX);
+        distance[S] = 0;
+        pq.push({0,S});
+        
+        while(!pq.empty())
+        {
+            int dist = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            
+            for(auto &x : adj[node])
+            {
+                int adjNode = x[0];
+                int nodeWeight = x[1];
+                
+                if(dist+nodeWeight < distance[adjNode])
+                {
+                    distance[adjNode] = dist+nodeWeight;
+                    pq.push({distance[adjNode],adjNode});
+                }
+            }
+        }
+        return distance;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        int S;
+        cin>>S;
+        
+        Solution obj;
+    	vector<int> res = obj.dijkstra(V, adj, S);
+    	
+    	for(int i=0; i<V; i++)
+    	    cout<<res[i]<<" ";
+    	cout<<endl;
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
