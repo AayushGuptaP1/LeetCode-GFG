f2df058b21e4b473914e35c6660917c0c8b22d5a
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-03-06 09:39:42 -0800

    Time: 24 ms (83.87%), Space: 13.2 MB (79.06%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
+    {
+        vector<vector<pair<int,int>>> graph(n);
+        vector<int> distance(n,INT_MAX);
+        distance[src] = 0;
+        int stop = 0;
+        int res = INT_MAX;
+        
+        for(auto &it : flights)
+        {
+            graph[it[0]].push_back({it[1],it[2]});
+        }
+        
+        queue<pair<int,int>>pq;
+        pq.push({0,src});
+        
+        while(!pq.empty() && stop<=k)
+        {
+            
+            int size = pq.size();
+            for(int i=0;i<size;++i)
+            {
+                int dist = pq.front().first;
+                int node = pq.front().second;
+                pq.pop();
+                
+                for(auto &it : graph[node])
+                {
+                    int adjNode = it.first;
+                    int edgeWeight = it.second;
+                    
+                    if(dist+edgeWeight < distance[adjNode])
+                    {
+                        distance[adjNode] = dist+edgeWeight;
+                        
+                        pq.push({distance[adjNode],adjNode});
+                    }
+                }
+            }
+            stop++;
+        }
+        return distance[dst]==INT_MAX? -1 : distance[dst];
+    }
+};
