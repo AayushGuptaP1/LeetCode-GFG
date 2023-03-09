0d14de4c2325ce7024627d27dbcf215b799948d4
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-03-09 02:05:18 -0800

    Time: 11 ms (49.72%), Space: 7.6 MB (84.91%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -10,50 +10,48 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) 
     {
-        ListNode *fast = head, *slow = head;
-        ListNode *meet = NULL;
         
-        while(fast && fast->next)
-        {
-            slow = slow->next;
-            fast = fast->next->next;
-            if(slow == fast)
-            {
-                meet = slow;
-                break;
-            }
-               
-        }
-        if(meet == NULL)
-            return NULL;
-        
-        slow = head;
-        while(slow != fast)
-        {
-            slow = slow->next;
-            fast = fast->next;
-        }
-        return slow;
-//         if(head == NULL || head->next == NULL)
-//             return NULL;
 //         ListNode *fast = head, *slow = head;
+//         ListNode *meet = NULL;
         
-//         do
+//          while(fast && fast->next)
 //         {
+//             fast = fast->next->next;
 //             slow = slow->next;
-//             fast = fast->next;
-//             if(fast->next)
-//                 fast = fast->next;
+            
+//             if(slow == fast)
+//             break;
 //         }
-//         while(slow != fast && fast->next != NULL);
-//             if(fast->next == NULL)
-//                 return NULL;
+        
+//         if(fast == NULL)
+//         return NULL;
 //         slow = head;
-//         while(slow != fast)
+//         while(fast != slow)
 //         {
-//             slow = slow->next;
 //             fast = fast->next;
+//             slow = slow->next;
 //         }
-//         return slow;
+//         return fast;
+       if(head == NULL || head->next == NULL)
+             return NULL;
+        ListNode *fast = head, *slow = head;
+        
+         do
+        {
+            slow = slow->next;
+             fast = fast->next;
+             if(fast->next)
+                 fast = fast->next;
+         }
+         while(slow != fast && fast->next != NULL);
+             if(fast->next == NULL)
+                 return NULL;
+         slow = head;
+         while(slow != fast)
+         {
+             slow = slow->next;
+             fast = fast->next;
+         }
+         return slow;
     }
 };
