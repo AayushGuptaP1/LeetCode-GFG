e3d4adc7f8a4b42a3802ef93fbdb540b827ffb9a
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-03-25 14:47:53 +0530

    Time: 453 ms (78.24%), Space: 130.6 MB (61.28%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    void dfs(int source, vector<vector<pair<int,int>>> &graph, vector<bool> &visited, int &res)
+    {
+        visited[source] = true;
+        
+        for(auto &x : graph[source])
+        {
+            int adjNode = x.first;
+            int edWt = x.second;
+            
+            res = min(res,edWt); 
+            
+            if(visited[adjNode]==false)
+                dfs(adjNode,graph,visited,res); 
+        }
+        
+    }
+public:
+    int minScore(int n, vector<vector<int>>& roads) 
+    {
+        int res = INT_MAX;
+        vector<vector<pair<int,int>>> graph(n+1);
+        
+        for(auto &x : roads)
+        {
+            graph[x[0]].push_back({x[1],x[2]});
+            graph[x[1]].push_back({x[0],x[2]});
+        }
+        
+        vector<bool> visited(n+1,false);
+        
+        dfs(1,graph,visited,res);
+        return res;
+    }
+};
