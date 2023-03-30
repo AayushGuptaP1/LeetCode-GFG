d8171c00adaedcc5283f9d85d0dfeb0d3c466e9a
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-03-30 23:03:25 +0530

    Time: 3 ms (73.55%), Space: 7.4 MB (88.03%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    void powerSet(vector<int>&nums,int index,vector<int> &current, vector<vector<int>> &ans)
+    {
+        ans.push_back(current);
+        
+        
+        for(int i=index;i<nums.size();++i)
+        {
+            if(i==index || nums[i] != nums[i-1])
+            {
+                current.push_back(nums[i]);
+                powerSet(nums,i+1,current,ans);
+                current.pop_back();
+            }
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums)
+    {
+        sort(begin(nums),end(nums));
+        vector<int> current;
+        vector<vector<int>> ans;
+        powerSet(nums,0,current,ans);
+        return ans;
+        
+        
+    }
+};
