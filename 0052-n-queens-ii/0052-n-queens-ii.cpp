227acbbc7a4e4cf1db00bff419b74e172ff5876f
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-04-01 10:58:20 +0530

    Time: 4 ms (77.20%), Space: 6 MB (82.59%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    void placeQueens(int n, int col,int &count,vector<int> &seq,vector<int> &leftRow, vector<int> &leftDiagonal, vector<int> &rightDiagonal)
+    void placeQueens(int n, int col,int &count,vector<int> &leftRow, vector<int> &leftDiagonal, vector<int> &rightDiagonal)
     {
         
         
@@ -15,9 +15,9 @@ private:
             if(!leftRow[row] && !leftDiagonal[n-1+col-row] && !rightDiagonal[row+col])
             {
                 leftRow[row] = leftDiagonal[n-1+col-row] = rightDiagonal[row+col] = 1; 
-                seq.push_back(row);
-                placeQueens(n,col+1,count,seq,leftRow,leftDiagonal,rightDiagonal);
-                seq.pop_back();
+                
+                placeQueens(n,col+1,count,leftRow,leftDiagonal,rightDiagonal);
+                
                 leftRow[row] = leftDiagonal[n-1+col-row] = rightDiagonal[row+col] = 0;
                 
             }
@@ -27,11 +27,11 @@ public:
     int totalNQueens(int n) 
     {
         int res = 0;        
-        vector<int> seq;
+        
         vector<int> leftRow(n,0);
         vector<int> leftDiagonal(2*n-1,0);
         vector<int> rightDiagonal(2*n-1,0);
-        placeQueens(n,0,res,seq,leftRow,leftDiagonal,rightDiagonal);
+        placeQueens(n,0,res,leftRow,leftDiagonal,rightDiagonal);
         return res;
     }
 };
