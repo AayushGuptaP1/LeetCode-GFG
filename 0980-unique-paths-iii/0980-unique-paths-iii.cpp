e675a33dde2abc03a34465966d317a0c2d825c5e
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-04-24 01:11:37 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (99.30%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+private:
+    void dfs(int i,int j, vector<vector<int>> &grid, int canWalk, int &count)
+    {
+        if(i<0 || i>= grid.size() || j<0 || j>=grid[0].size()||grid[i][j] == -1)
+            return;
+        --canWalk;
+        if(grid[i][j] == 2)
+        {
+            if(canWalk == 0)
+                ++count;
+            return;
+        }
+        grid[i][j] = -1;
+        
+        dfs(i-1,j,grid,canWalk,count);
+        dfs(i+1,j,grid,canWalk,count);
+        dfs(i,j-1,grid,canWalk,count);
+        dfs(i,j+1,grid,canWalk,count);
+        grid[i][j] = 0;
+    }
+public:
+    int uniquePathsIII(vector<vector<int>>& grid) 
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        int canWalk = m*n;
+        int count = 0;
+        
+        for(int i=0;i<m;++i)
+        {
+            for(int j=0;j<n;++j)
+            {
+                if(grid[i][j] == -1)
+                    --canWalk;
+            }
+        }
+        
+        for(int i=0;i<m;++i)
+        {
+            for(int j=0;j<n;++j)
+            {
+                if(grid[i][j] == 1)
+                    dfs(i,j,grid,canWalk,count);
+            }
+        }
+        return count;
+    }
+};
