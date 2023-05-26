2450093270e09e329ff97ff79dea96a6d4af1110
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-05-26 17:38:37 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.cpp
+++ Stickler Thief - GFG/stickler-thief.cpp
@@ -6,23 +6,23 @@ typedef long long int ll;
 // } Driver Code Ends
 class Solution
 {
-    private:
-    int maxRob(int nums[], int index,vector<int> &dp)
-    {
-        if(index < 0)
-            return 0;
-        if(dp[index] != -1)
-            return dp[index];
-        
-        return dp[index] =  max(maxRob(nums,index-2,dp)+nums[index],maxRob(nums,index-1,dp));
-    }
-    public:
+  public:
     //Function to find the maximum money the thief can get.
     int FindMaxSum(int nums[], int n)
     {
-         vector<int> dp(n+1, -1);
-        int index = n-1;
-        return maxRob(nums,index,dp);
+        vector<int> dp(n+1, -1);
+        
+        dp[0] = nums[0];
+        
+        for(int i=1;i<n;++i)
+        {
+            int even = dp[i-1];
+            int odd = 0;
+            if(i-2>=0)
+                odd = dp[i-2];
+            dp[i] = max(odd + nums[i], even);
+        }
+        return dp[n-1];
     }
 };
 
