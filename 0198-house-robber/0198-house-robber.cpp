a160e46f50439e2323a1d1060369818b025cd5ac
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-05-26 17:39:34 +0530

    Time: 5 ms (28.00%), Space: 7.8 MB (53.87%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,5 @@
 class Solution {
-private:
-    int maxRob(vector<int> &nums, int index,vector<int> &dp)
-    {
-        if(index < 0)
-            return 0;
-        if(dp[index] != -1)
-            return dp[index];
-        
-        return dp[index] =  max(maxRob(nums,index-2,dp)+nums[index],maxRob(nums,index-1,dp));
-    }
+
 public:
     int rob(vector<int>& nums) 
     {
