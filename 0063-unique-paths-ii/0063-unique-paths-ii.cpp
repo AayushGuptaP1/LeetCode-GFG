41e971cb4bcbd3202ed13245b6fcaad3dda7c52e
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-06-01 21:50:12 +0530

    Time: 4 ms (49.42%), Space: 7.8 MB (35.89%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -14,9 +14,7 @@ public:
             for(int j=0;j<n;++j)
             {
                 if(obstacleGrid[i][j] == 1)
-                    dp[i][j] = 0;
-                else if(i==0 && j== 0)
-                    dp[i][j] = 1;                
+                    dp[i][j] = 0;              
                 else
                 {                  
                     if(i>0)
