5c1e004689c97a96b43126c1230c892b72ad7f88
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-06-01 21:51:48 +0530

    Time: 4 ms (49.42%), Space: 7.6 MB (80.57%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -5,8 +5,8 @@ public:
     {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,0));
-        dp[0][0] = 1;
+        vector<int>dp(n,0);
+        dp[0] = 1;
             
         
         for(int i=0;i<m;++i)
@@ -14,18 +14,17 @@ public:
             for(int j=0;j<n;++j)
             {
                 if(obstacleGrid[i][j] == 1)
-                    dp[i][j] = 0;              
+                    dp[j] = 0;              
                 else
                 {                  
-                    if(i>0)
-                        dp[i][j] += dp[i-1][j];
                     if(j>0)
-                        dp[i][j] += dp[i][j-1];
+                        dp[j] += dp[j-1];
+                    
                 }
                     
             }
         }
         
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 };
