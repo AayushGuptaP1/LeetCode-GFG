5a028fd4068cae04c7d63469dfbb1ef6857d2bd7
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-06-10 20:42:09 +0530

    Time: 4 ms (84.00%), Space: 8.9 MB (49.75%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,29 +1,30 @@
 class Solution {
-private:
-    int findMin(int m, int n, vector<vector<int>> &triangle,vector<vector<int>> &dp)
-    {
-        if(n<0 ||n == triangle[m].size() )
-            return INT_MAX;
-        
-        if(m == 0)
-            return triangle[0][0];
-        if(dp[m][n] != -1)
-            return dp[m][n];
-        
-        return dp[m][n] =  min(findMin(m-1,n-1,triangle,dp),findMin(m-1,n,triangle,dp))+ triangle[m][n];
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle)
     {
         int m = triangle.size();
-        int n = triangle[m-1].size();        
+        vector<vector<int>> dp(m,vector<int>(m,0));
+        dp[0][0] = triangle[0][0];
         int res = INT_MAX;
-        vector<vector<int>> dp(m, vector<int>(n,-1));
         
-        for(int i=0;i<n;++i)
+        for(int i=1;i<m;++i)
         {
-            res = min(res, findMin(m-1,i,triangle,dp));
+            dp[i][0] = dp[i-1][0] + triangle[i][0];
+            dp[i][i] = dp[i-1][i-1] + triangle[i][i];
         }
-        return res;        
+        
+        for(int i=1;i<m;++i)
+        {
+            for(int j=1;j<i;++j)
+            {
+                dp[i][j] = min(dp[i-1][j-1],dp[i-1][j]) + triangle[i][j];
+            }
+        }
+        
+        for(int i=0;i<m;++i)
+        {
+            res = min(res, dp[m-1][i]);
+        }
+        return res;
     }
 };
