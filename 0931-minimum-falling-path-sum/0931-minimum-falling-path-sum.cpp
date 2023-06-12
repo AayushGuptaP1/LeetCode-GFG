d6f6842b735d316de3e5bdfc22d5bfbec4ebb21c
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-06-12 21:27:32 +0530

    Time: 17 ms (55.42%), Space: 10.2 MB (50.07%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -5,23 +5,24 @@ public:
         int m = matrix.size();
         int res = INT_MAX;
         
-        vector<vector<int>> dp(m,vector<int>(m,0));
+        vector<int>curr(m,0), prev(m,0);
         
         for(int i=0;i<m;++i)
-            dp[0][i] = matrix[0][i];       
+            prev[i] = matrix[0][i];       
         
         for(int i=1;i<m;++i)
         {
             for(int j=0;j<m;++j)
             {
-                int leftD = (j>0)? dp[i-1][j-1] : INT_MAX;
-                int rightD = (j<m-1)? dp[i-1][j+1] : INT_MAX;
-                dp[i][j] = min(dp[i-1][j],min(leftD,rightD)) + matrix[i][j];               
-            }                
+                int leftD = (j>0)? prev[j-1] : INT_MAX;
+                int rightD = (j<m-1)? prev[j+1] : INT_MAX;
+                curr[j] = min(prev[j],min(leftD,rightD)) + matrix[i][j];               
+            }  
+            prev = curr;
         }
         
         for(int i=0;i<m;++i)
-            res = min(res, dp[m-1][i]);        
+            res = min(res, prev[i]);        
         
         return res;
         
