5d85ccbc6f3033ac58ed078b78baa249a34833bf
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-06-18 18:44:56 +0530

    Time: 290 ms (10.21%), Space: 16.9 MB (72.21%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,39 +1,56 @@
 class Solution {
-private:
-    int findMaxCherry(int i, int j1, int j2, int m, int n, vector<vector<int>> &grid,vector<vector<vector<int>>> &dp)
-    {
-        if(j1<0 || j1==n || j2<0 || j2 ==n)
-            return INT_MIN;
-        if(i == m-1)
-        {
-            if(j1 == j2)
-                return grid[i][j1];
-            else
-                return grid[i][j1]+grid[i][j2];
-        }
-        if(dp[i][j1][j2] != -1)
-            return dp[i][j1][j2];
-        int res = INT_MIN;
-        
-        for(int dj1 = -1;dj1<=1;++dj1)
-        {
-            for(int dj2 = -1;dj2<=1;++dj2)
-            {
-                if(j1 == j2)
-                    res = max(res, findMaxCherry(i+1,j1+dj1,j2+dj2,m,n,grid,dp) + grid[i][j1]);
-                else
-                    res =  max(res, findMaxCherry(i+1,j1+dj1,j2+dj2,m,n,grid,dp)+ grid[i][j1]+ grid[i][j2]);
-            }
-        }
-        return dp[i][j1][j2] = res;
-    }
 public:
     int cherryPickup(vector<vector<int>>& grid) 
     {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<vector<int>>> dp(m,vector<vector<int>>(n,vector<int>(n,-1)));
+        vector<vector<vector<int>>> dp(m,vector<vector<int>>(n,vector<int>(n,0)));
         
-        return findMaxCherry(0,0,n-1,m,n,grid,dp);
+        for(int j1=0;j1<n;++j1)
+        {
+            for(int j2=0;j2<n;++j2)
+            {
+                if(j1 == j2)
+                    dp[m-1][j1][j2] = grid[m-1][j1];
+                else
+                    dp[m-1][j1][j2] = grid[m-1][j1] + grid[m-1][j2];
+            }
+        }
+        
+        for(int i=m-2;i>=0;--i)
+        {
+            for(int j1=0;j1<n;++j1)
+            {
+                for(int j2=0;j2<n;++j2)
+                {
+                    int res = INT_MIN;
+                    
+                    for(int dj1 = -1;dj1<=1;++dj1)
+                    {
+                        for(int dj2 = -1;dj2<=1;++dj2)
+                        {
+                            int value = 0;
+                            if(j1 == j2)
+                                value = grid[i][j1];
+                            else
+                                value = grid[i][j1] + grid[i][j2];
+                            
+                            if(j1+dj1>=0 && j1+dj1<n && j2+dj2>=0 && j2+dj2<n)
+                                value += dp[i+1][j1+dj1][j2+dj2];
+                            else
+                                value += -1e8;
+                            
+                            res = max(res,value);                           
+                                
+                        }
+                    }
+                    dp[i][j1][j2] = res;
+                    
+                }
+            }
+        }
+        return dp[0][0][n-1];
+            
+        
     }
 };
