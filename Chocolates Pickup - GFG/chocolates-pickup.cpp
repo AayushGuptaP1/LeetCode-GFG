b01eb7770c65fda622e6896fe5113c1e161c6e9d
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-06-18 18:50:57 +0530

    Added solution - LeetHub

--- Chocolates Pickup - GFG/chocolates-pickup.cpp
+++ Chocolates Pickup - GFG/chocolates-pickup.cpp
@@ -5,40 +5,57 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-  private:
-  int findMaxChocolates(int i, int j1, int j2, int m, int n, vector<vector<int>> &grid,vector<vector<vector<int>>>& dp)
-  {
-      if(j1<0 || j1 == n || j2<0 || j2 == n)
-      return INT_MIN;
-      
-      if(i == m-1)
-      {
-          if(j1 == j2)
-          return grid[i][j1];
-          else
-          return grid[i][j1]+grid[i][j2];
-      }
-      if(dp[i][j1][j2] != -1)
-            return dp[i][j1][j2];
-      int res = INT_MIN;
-      
-      for(int dj1= -1;dj1<=1;++dj1)
-      {
-          for(int dj2= -1;dj2<=1;++dj2)
-          {
-              if(j1 == j2)
-              res = max(res, findMaxChocolates(i+1,j1+dj1,j2+dj2,m,n,grid,dp)+ grid[i][j1]) ;
-              else
-              res = max(res, findMaxChocolates(i+1,j1+dj1,j2+dj2,m,n,grid,dp)+ grid[i][j1]+grid[i][j2]) ;
-          }
-      }
-      return dp[i][j1][j2] = res;
-  }
   public:
     int solve(int m, int n, vector<vector<int>>& grid)
     {
-        vector<vector<vector<int>>> dp(m,vector<vector<int>>(n,vector<int>(n,-1)));
-        return findMaxChocolates(0,0,n-1,m,n,grid,dp);
+        
+        vector<vector<int>> prev(n,vector<int>(n,0)), curr(n,vector<int>(n,0));
+        
+        for(int j1=0;j1<n;++j1)
+        {
+            for(int j2=0;j2<n;++j2)
+            {
+                if(j1 == j2)
+                    prev[j1][j2] = grid[m-1][j1];
+                else
+                    prev[j1][j2] = grid[m-1][j1] + grid[m-1][j2];
+            }
+        }
+        
+        for(int i=m-2;i>=0;--i)
+        {
+            for(int j1=0;j1<n;++j1)
+            {
+                for(int j2=0;j2<n;++j2)
+                {
+                    int res = INT_MIN;
+                    
+                    for(int dj1 = -1;dj1<=1;++dj1)
+                    {
+                        for(int dj2 = -1;dj2<=1;++dj2)
+                        {
+                            int value = 0;
+                            if(j1 == j2)
+                                value = grid[i][j1];
+                            else
+                                value = grid[i][j1] + grid[i][j2];
+                            
+                            if(j1+dj1>=0 && j1+dj1<n && j2+dj2>=0 && j2+dj2<n)
+                                value += prev[j1+dj1][j2+dj2];
+                            else
+                                value += -1e8;
+                            
+                            res = max(res,value);                           
+                                
+                        }
+                    }
+                    curr[j1][j2] = res;
+                    
+                }
+            }
+            prev = curr;
+        }
+        return curr[0][n-1];
     }
 };
 
