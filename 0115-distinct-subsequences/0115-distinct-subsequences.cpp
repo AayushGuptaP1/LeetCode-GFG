8a47f02c41668bce23722d53f572edd265ea7e38
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-09-12 22:08:34 +0530

    Time: 33 ms (67.23%), Space: 18.5 MB (55.79%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -2,16 +2,16 @@ class Solution {
 private:
     int countDistinct(int i, int j, string &s, string &t,vector<vector<int>> &dp)
     {
-        if(j<0)
+        if(j==0)
             return 1;
-        if(i<0)
+        if(i==0)
             return 0;
         if(dp[i][j] != -1)
             return dp[i][j];
         
         int notTake = countDistinct(i-1,j,s,t,dp);
         int take = 0;
-        if(s[i] == t[j])
+        if(s[i-1] == t[j-1])
             take =  countDistinct(i-1,j-1,s,t,dp);
         return dp[i][j] = take+notTake;
     }
@@ -20,8 +20,8 @@ public:
     {
         int m = s.length();
         int n = t.length();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return countDistinct(m-1,n-1,s,t,dp);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        return countDistinct(m,n,s,t,dp);
         
     }
 };
