5a35c5bc2f04020f7b571a84cbe54d7205cfc8d8
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-09-14 01:13:11 +0530

    Time: 11 ms (64.60%), Space: 6.7 MB (90.43%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,27 +1,35 @@
 class Solution {
-private:
-    int findDistance(string &word1, string &word2, int i,int j,vector<vector<int>> &dp)
-    {
-        if(i==0)
-            return j;
-        if(j==0)
-            return i;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        if(word1[i-1] == word2[j-1])
-            return dp[i][j] = findDistance(word1,word2,i-1,j-1,dp);
-        int insert = 1+findDistance(word1,word2,i,j-1,dp);
-        int update = 1+ findDistance(word1,word2,i-1,j-1,dp);
-        int del = 1+ findDistance(word1,word2,i-1,j,dp);
-        
-        return dp[i][j] =  min(insert,min(update,del));
-    }
 public:
     int minDistance(string word1, string word2)
     {
         int m = word1.length();
         int n = word2.length();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return findDistance(word1,word2,m,n,dp);
+        vector<int> prev(n+1,0),curr(n+1,0);
+        curr[0] = 0;
+        
+        
+        for(int j=0;j<=n;++j)
+        {
+            prev[j] = j;
+        }
+        for(int i=1;i<=m;++i)
+        {
+            curr[0] = i;
+            for(int j=1;j<=n;++j)
+            {
+                if(word1[i-1] == word2[j-1])
+                    curr[j] = prev[j-1];
+                else
+                {
+                    int insert = 1+curr[j-1];
+                    int update = 1+prev[j-1];
+                    int del = 1+prev[j];
+        
+                    curr[j] =  min(insert,min(update,del));
+                }
+            }
+            prev = curr;
+        }
+        return prev[n];
     }
 };
