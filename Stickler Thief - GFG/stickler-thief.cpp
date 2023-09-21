a21cfa8df43a75ef2fc9da37dd3f11ec949e73a5
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-09-21 13:04:13 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.cpp
+++ Stickler Thief - GFG/stickler-thief.cpp
@@ -19,7 +19,7 @@ class Solution
             int even = dp[i-1];
             int odd = 0;
             if(i-2>=0)
-                odd = dp[i-2] ;
+                odd = dp[i-2]  ;
             dp[i] = max(odd+ nums[i], even);
         }
         return dp[n-1];
