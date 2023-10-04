5db6aa869f9d5d103b9be49845db7fbcefec9742
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-10-04 20:17:05 +0530

    Time: 44 ms (58.80%), Space: 28.2 MB (8.20%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -2,26 +2,27 @@ class Solution {
 private:
     bool canMatch(string &s, string &p, int i, int j,vector<vector<int>> &dp)
     {
-        if(i<0 && j<0)
+        if(i==0 && j==0)
             return true;
-        if(i<0 && j>=0)
+        if(j==0 && i>0)
+            return false;
+        if(i==0 && j>0)
         {
-            for(int k=0;k<=j;++k)
+            for(int k=1;k<=j;++k)
             {
-                if(p[k] != '*')
+                if(p[k-1] != '*')
                     return false;
             }
             return true;
         }
-        if(j<0)
-            return false;
+        
         
         if(dp[i][j] != -1)
             return dp[i][j];
         
-        if(s[i] == p[j] || p[j] == '?')
+        if(s[i-1] == p[j-1] || p[j-1] == '?')
             return dp[i][j] =  canMatch(s,p,i-1,j-1,dp);
-        else if(p[j] == '*')
+        else if(p[j-1] == '*')
         {
             bool take = canMatch(s,p,i,j-1,dp);
             bool notTake = canMatch(s,p,i-1,j,dp);
@@ -35,8 +36,8 @@ public:
     {
         int m = s.length();
         int n = p.length();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
         
-        return canMatch(s,p,m-1,n-1,dp);
+        return canMatch(s,p,m,n,dp);
     }
 };
