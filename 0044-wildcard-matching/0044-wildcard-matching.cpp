17805a303c7129eac0d7cb174dd1e71b146983d4
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-10-04 21:12:14 +0530

    Time: 105 ms (15.63%), Space: 11.8 MB (62.70%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -36,8 +36,34 @@ public:
     {
         int m = s.length();
         int n = p.length();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        vector<vector<bool>> dp(m+1,vector<bool>(n+1,false));        
+        dp[0][0] = true;
+        for(int j=1;j<=n;++j)
+        {
+            if(p[j-1] == '*')
+                dp[0][j]= true;
+            else
+                break;
+        }
         
-        return canMatch(s,p,m,n,dp);
+        for(int i=1;i<=m;++i)
+        {
+            for(int j=1;j<=n;++j)
+                {
+                    if(s[i-1] == p[j-1] || p[j-1] == '?')
+                        dp[i][j] =  dp[i-1][j-1];
+                    else if(p[j-1] == '*')
+                    {
+                        bool take = dp[i][j-1];
+                        bool notTake = dp[i-1][j];
+                        dp[i][j] =take | notTake;
+                    }
+                    else
+                        dp[i][j] =  false;
+                }
+        }
+        
+        
+        return dp[m][n];
     }
 };
