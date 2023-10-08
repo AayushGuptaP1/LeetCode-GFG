5b6208f44d5cdf059810793d1ad79f78d21d22cd
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-10-08 23:46:15 +0530

    Time: 11 ms (72.12%), Space: 11.1 MB (82.73%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,24 +1,24 @@
 class Solution {
-private:
-    int findProfit(vector<int> &prices, int buy, int limit, int index, int size,vector<vector<vector<int>>> &dp)
-    {
-        if(index == size || limit == 0)
-            return 0;
-        if(dp[index][buy][limit] != -1)
-            return dp[index][buy][limit];
-        int profit = 0;
-        if(buy && limit)
-        {
-            profit = max(-prices[index] + findProfit(prices,0,limit,index+1,size,dp),0+findProfit(prices,1,limit,index+1,size,dp));
-        }
-        else if(buy ==0 && limit)
-        {
-            profit = max(prices[index]+findProfit(prices,1,limit-1,index+1,size,dp),0+findProfit(prices,0,limit,index+1,size,dp));
-        }
-        return dp[index][buy][limit] =  profit;
+// private:
+//     int findProfit(vector<int> &prices, int buy, int limit, int index, int size,vector<vector<vector<int>>> &dp)
+//     {
+//         if(index == size || limit == 0)
+//             return 0;
+//         if(dp[index][buy][limit] != -1)
+//             return dp[index][buy][limit];
+//         int profit = 0;
+//         if(buy && limit)
+//         {
+//             profit = max(-prices[index] + findProfit(prices,0,limit,index+1,size,dp),0+findProfit(prices,1,limit,index+1,size,dp));
+//         }
+//         else if(buy ==0 && limit)
+//         {
+//             profit = max(prices[index]+findProfit(prices,1,limit-1,index+1,size,dp),0+findProfit(prices,0,limit,index+1,size,dp));
+//         }
+//         return dp[index][buy][limit] =  profit;
             
         
-    }
+//     }
 public:
     int maxProfit(int k, vector<int>& prices) 
     {
