268935f27dee9bf5ce4a9ae7c7db1339a3b1d535
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-10-08 23:37:56 +0530

    Time: 139 ms (77.38%), Space: 75.7 MB (79.83%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,29 +1,27 @@
 class Solution {
-private:
-    int findProfit(vector<int> &prices, int buy, int limit, int index, int size,vector<vector<vector<int>>> &dp)
-    {
-        if(index == size || limit == 0)
-            return 0;
-        if(dp[index][buy][limit] != -1)
-            return dp[index][buy][limit];
-        int profit = 0;
-        if(buy && limit)
-        {
-            profit = max(-prices[index] + findProfit(prices,0,limit,index+1,size,dp),0+findProfit(prices,1,limit,index+1,size,dp));
-        }
-        else if(buy ==0 && limit)
-        {
-            profit = max(prices[index]+findProfit(prices,1,limit-1,index+1,size,dp),0+findProfit(prices,0,limit,index+1,size,dp));
-        }
-        return dp[index][buy][limit] =  profit;
-            
-        
-    }
+// private:
+//     int findProfit(vector<int> &prices, int buy, int limit, int index, int size,vector<vector<vector<int>>> &dp)
+//     {
+//         if(index == size || limit == 0)
+//             return 0;
+//         if(dp[index][buy][limit] != -1)
+//             return dp[index][buy][limit];
+//         int profit = 0;
+//         if(buy && limit)
+//         {
+//             profit = max(-prices[index] + findProfit(prices,0,limit,index+1,size,dp),0+findProfit(prices,1,limit,index+1,size,dp));
+//         }
+//         else if(buy ==0 && limit)
+//         {
+//             profit = max(prices[index]+findProfit(prices,1,limit-1,index+1,size,dp),0+findProfit(prices,0,limit,index+1,size,dp));
+//         }
+//         return dp[index][buy][limit] =  profit;    
+//     }
 public:
     int maxProfit(vector<int>& prices) 
     {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        vector<vector<int>> curr(2,vector<int>(3,0));
         
         for(int index = n-1;index>=0;--index)
         {
@@ -34,17 +32,17 @@ public:
                     int profit = 0;
                     if(buy)
                     {
-                        profit = max(-prices[index]+dp[index+1][0][limit],dp[index+1][1][limit]);
+                        profit = max(-prices[index]+curr[0][limit],curr[1][limit]);
                     }
                     else
                     {
-                        profit = max(prices[index]+ dp[index+1][1][limit-1],dp[index+1][0][limit]);
+                        profit = max(prices[index]+ curr[1][limit-1],curr[0][limit]);
                     }
-                    dp[index][buy][limit] = profit;
+                    curr[buy][limit] = profit;
                 }
             }
         }
         
-        return dp[0][1][2];
+        return curr[1][2];
     }
 };
