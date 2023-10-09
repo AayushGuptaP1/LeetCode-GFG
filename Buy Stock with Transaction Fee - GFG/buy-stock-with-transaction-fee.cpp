77eb38f1cabc6e444e1c2aa52f839b3cb010d301
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-10-09 21:36:47 +0530

    Added solution - LeetHub

---
+++ Buy Stock with Transaction Fee - GFG/buy-stock-with-transaction-fee.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long maximumProfit(vector<long long>&prices, int n, int fee) 
+    {
+       long long buy = 0, sell = 0;
+        
+        for(long long index = n-1;index>=0;index--)
+        {
+            buy = max(-prices[index]+ sell,buy);
+            sell =  max(prices[index]-fee+buy,sell);
+        }
+        
+        return buy;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vector<long long> prices;
+        
+        for(int i=0; i<n; ++i){
+            long long x; cin>>x;
+            prices.push_back(x);
+        }
+        
+        int fee; cin>>fee;
+        
+        Solution obj;
+        cout<<obj.maximumProfit(prices, n, fee)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
