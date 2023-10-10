abb4c6a5a5e38322a44e35a07177c8930f3b3894
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-10-10 13:33:41 +0530

    Time: 509 ms (28.41%), Space: 292.5 MB (8.14%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,22 +1,34 @@
 class Solution {
-private:
-    int findLIS(vector<int> &nums,int index, int size, int lastIndex,vector<vector<int>> &dp)
-    {
-        if(index == size)
-            return 0;
-        if(dp[index][lastIndex+1] != -1)
-            return dp[index][lastIndex+1];
-        int take = 0;
-        if(lastIndex == -1 || nums[index] > nums[lastIndex])
-            take = 1 + findLIS(nums,index+1,size,index,dp);
-        int notTake = findLIS(nums,index+1,size,lastIndex,dp);
-        return dp[index][lastIndex+1] =  max(take,notTake);
-    }
+// private:
+//     int findLIS(vector<int> &nums,int index, int size, int lastIndex,vector<vector<int>> &dp)
+//     {
+//         if(index == size)
+//             return 0;
+//         if(dp[index][lastIndex+1] != -1)
+//             return dp[index][lastIndex+1];
+//         int take = 0;
+//         if(lastIndex == -1 || nums[index] > nums[lastIndex])
+//             take = 1 + findLIS(nums,index+1,size,index,dp);
+//         int notTake = findLIS(nums,index+1,size,lastIndex,dp);
+//         return dp[index][lastIndex+1] =  max(take,notTake);
+//     }
 public:
     int lengthOfLIS(vector<int>& nums) 
     {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));        
-        return findLIS(nums,0,n,-1,dp);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0)); 
+        
+        for(int index=n-1;index>=0;--index)
+        {
+            for(int lastIndex=-1;lastIndex<index;++lastIndex)
+            {
+                int take = 0;
+                if(lastIndex == -1 || nums[index] > nums[lastIndex])
+                    take = 1 + dp[index+1][index+1];
+                int notTake = dp[index+1][lastIndex+1];
+                dp[index][lastIndex+1] =  max(take,notTake);                
+            }
+        }
+        return dp[0][0];
     }
 };
