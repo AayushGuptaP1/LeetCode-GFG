85b55951d14405999b13c55727e565b69ab39b6c
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2023-11-17 13:57:37 +0530

    Time: 3 ms (40.97%), Space: 6.3 MB (73.38%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int trailingZeroes(int n) 
+    {
+        int res = 0;
+        
+        for(int i=5;i<=n;i*=5)
+        {
+            res += n/i;
+        }
+        return res;        
+    }
+};
