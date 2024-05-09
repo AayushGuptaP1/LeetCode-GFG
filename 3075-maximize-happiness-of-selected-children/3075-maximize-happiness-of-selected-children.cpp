815f70a21e0d5c13f8c60075dfbc0d744eb07e2d
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2024-05-09 10:07:07 +0530

    Time: 141 ms (96.96%), Space: 107.2 MB (60.15%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -4,13 +4,12 @@ public:
     {
         int n = happiness.size();
         long long res = 0;
-        priority_queue<int> pq(begin(happiness),end(happiness));
+        sort(begin(happiness),end(happiness),greater<int>());
         
         
         for(int i=0;i<k;++i)
         {
-            res += max(0,pq.top()-i);
-            pq.pop(); 
+            res += max(0,happiness[i]-i);
         }
         return res;
         
