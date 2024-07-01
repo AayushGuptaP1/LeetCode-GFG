0fc6afe5604ccc62d9f4bbc643093153a2d03afd
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2024-07-01 08:03:24 +0530

    Time: 3 ms (60.88%), Space: 10.6 MB (8.09%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -5,10 +5,9 @@ public:
         int n = arr.size();
         
         for(int i=1;i<n-1;++i)
-        {
             if(arr[i-1] & 1 & arr[i] & arr[i+1])
                 return true;
-        }
+        
         return false;        
     }
 };
