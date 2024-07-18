b515bf2605c2cc61573c63a7a585e2e83c74047e
Aayush Gupta <77824396+AayushGuptaP1@users.noreply.github.com>
2024-07-19 00:01:50 +0530

    Time: 16 ms (56.71%), Space: 27.5 MB (17.76%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -0,0 +1,32 @@
+
+class Solution {
+private:
+    TreeNode* findForest(TreeNode *root, vector<TreeNode*>&res, unordered_set<int>&st)
+    {
+        if(root == nullptr)
+            return root;
+        root->left = findForest(root->left,res,st);
+        root->right = findForest(root->right,res,st);
+        
+        if(st.find(root->val) != st.end())
+        {
+            if(root->left)
+                res.push_back(root->left);
+            if(root->right)
+                res.push_back(root->right);
+            delete root;
+            return nullptr;
+        }
+        return root;
+    }
+public:
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& del) 
+    {
+        vector<TreeNode*> res;
+        unordered_set<int> st(begin(del),end(del));
+        root = findForest(root,res,st);
+        if(root)
+            res.push_back(root);
+        return res;               
+    }
+};
